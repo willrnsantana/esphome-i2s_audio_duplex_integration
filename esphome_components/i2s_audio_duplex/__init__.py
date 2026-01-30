4862a95d21c0895d122c6a14e40801645ab32459
root <root@Notebook-Druid.localdomain>
2026-01-30 02:59:56 -0300

    Fix

--- esphome_components/i2s_audio_duplex/__init__.py
+++ esphome_components/i2s_audio_duplex/__init__.py
@@ -7,6 +7,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.const import CONF_ID, CONF_BITS_PER_SAMPLE, CONF_CHANNEL
+from esphome.cpp_generator import MockObjClass
 
 CODEOWNERS = ["@n-IA-hane"]
 DEPENDENCIES = []
